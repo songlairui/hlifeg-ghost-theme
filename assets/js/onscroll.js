f611960033a1c7f5009161dd79ae6216e0f66f38
Songlairui <cnre@live.cn>
2016-10-24 13:40:14 +0800

    增加滚动的TopBar

---
+++ assets/js/onscroll.js
@@ -0,0 +1,28 @@
+/**
+ * Created by cnre on 2016/10/24.
+ */
+(function(){
+    var timer=false;
+    var boundary=100;
+    var s = document.getElementById('FixedTopBar').style;
+    window.onscroll = function(ev) {
+        if(window.scrollY<boundary){
+            if(s.opacity!=0){s.opacity=0;};
+            clearTimeout(timer);
+            timer=setTimeout(
+                function(){
+                    (s.display!='none')?s.display='none':null;
+                },400);
+            //timer='aa';
+            //console.log(timer);
+        }else{
+            //console.log(timer);
+            if(timer){clearTimeout(timer)};
+            (s.display!='block')?s.display='block':null;
+            timer=setTimeout(
+                function(){
+                    (s.opacity!=1)?s.opacity=1:null;
+                },0);
+        }
+    };
+})();
