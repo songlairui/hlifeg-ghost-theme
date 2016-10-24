c87faaee2b06e41bed2cc1c3dd1d48a186b435fc
Songlairui <cnre@live.cn>
2016-10-24 23:00:42 +0800

    增加返回顶部

--- assets/js/onscroll.js
+++ assets/js/onscroll.js
@@ -5,13 +5,16 @@
     var timer=false;
     var boundary=162;
     var s = document.getElementById('FixedTopBar').style;
+    var s1 = document.getElementById('scroll2Top').style;
     window.onscroll = function(ev) {
         if(window.scrollY<boundary){
             if(s.opacity!=0){s.opacity=0;};
+            if(s1.opacity!=0){s1.opacity=0;};
             clearTimeout(timer);
             timer=setTimeout(
                 function(){
                     (s.display!='none')?s.display='none':null;
+                    (s1.display!='none')?s1.display='none':null;
                 },600);
             //timer='aa';
             //console.log(timer);
@@ -19,9 +22,11 @@
             //console.log(timer);
             if(timer){clearTimeout(timer)};
             (s.display!='block')?s.display='block':null;
+            (s1.display!='block')?s1.display='block':null;
             timer=setTimeout(
                 function(){
                     (s.opacity!=1)?s.opacity=1:null;
+                    (s1.opacity!=1)?s1.opacity=1:null;
                 },0);
         }
     };
